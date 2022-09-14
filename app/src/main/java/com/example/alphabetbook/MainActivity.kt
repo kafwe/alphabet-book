f97865034b743b5908c00b6fbc449fd231833656
Jordy Kafwe <kfwjor001@myuct.ac.za>
2022-09-14 21:05:58 +0200

    generate grid of buttons

--- app/src/main/java/com/example/alphabetbook/MainActivity.kt
+++ app/src/main/java/com/example/alphabetbook/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.alphabetbook
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linearLayoutVertical = findViewById<LinearLayout>(R.id.linear_layout_vert)
+        val row = 9
+        val col = 3
+        var buttonCount = 0
+
+
+        for (i in 1..row) {
+            val linearLayoutRow = LinearLayout(this)
+            linearLayoutRow.orientation = LinearLayout.HORIZONTAL
+            linearLayoutVertical.addView(linearLayoutRow)
+            for (j in 1..col) {
+                val button = Button(this)
+
+                linearLayoutRow.addView(button)
+
+                buttonCount++
+                if (buttonCount == 26) break
+            }
+        }
     }
 }
